b38f87f6524f6e38c3f7390988592d8cf8bf0cb0
Avelardo Valdez <avelardoval@gmail.com>
2020-01-18 03:04:49 -0800

    Add api keys

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,7 @@
 import * as firebase from "firebase/app";
 import "firebase/auth";
 
-const app = firebase.initalizeApp({
+const app = firebase.initializeApp({
     apiKey: process.env.REACT_APP_FIREBASE_KEY,
     authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
     databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
