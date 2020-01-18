15f83f58f33243e844de2642776210a7561503e6
Deamoriaz <deamoriaz@gmail.com>
2020-01-18 14:14:08 -0800

    Updated with css formatting.

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -21,21 +21,31 @@ function SignUp( {history} ) {
 
 
     return (
-        <div>
+        <div style={{marginTop:"5px",
+                    marginBottom:"5px",
+                    textAlign:"center"}}>
+        <meta name="viewport" content="width-device-width, initial-scale=1.0"></meta>
         <h2>The Crackalackin Sign Up via Google</h2>
         <form onSubmit={handleSignUp}>
             <label>
-                <p style={{marginTop:"5px", marginBottom:"5px"}}>Gmail</p>
-                <input name="email" type="email" placholder="Gmail Account" />
+                <p style={{marginTop:"5px",
+                            marginBottom:"5px",
+                            textAlign:"center"}}>Gmail</p>
+                <input name="email" type="email" placeholder="Gmail Account" />
             </label>
             <br />
             <label>
-                <p>Password</p>
-                <input name="password" type="password" placeholder="Password" />
+            <p style={{marginTop:"15px",
+                        marginBottom:"5px",
+                        textAlign:"center"}}>Password</p>
+                <input name="password"
+                    type="password"
+                    placeholder="Password"
+                    style={{width:"230px"}}/>
             </label>
-            <br />
-            <br />
-            <button type="submit">Sign Up</button>
+            <p style={{marginTop:"5px", marginBottom:"5px"}}>
+            <button type="submit" style={{width:"230px"}}>Sign Up </button> 
+            </p>
         </form>
         </div>
      );
