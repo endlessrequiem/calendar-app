066a5ecda0416fa94b76575fc10e98548774a1eb
Deamoriaz <deamoriaz@gmail.com>
2020-01-18 23:36:31 -0800

    Updated Event popup

--- src/components/Home/AddEventModal.jsx
+++ src/components/Home/AddEventModal.jsx
@@ -29,45 +29,31 @@ function AddEventModal(props) {
     <div className="modal fade" id={props.id} tabIndex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
       <div className="modal-dialog modal-dialog-centered" role="document">
         <div className="modal-content">
-          <div className="modal-header">
-            <h5 className="modal-title" id="exampleModalCenterTitle">Modal title</h5>
-            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+          <div className="modal-login" style={{backgroundColor:"lightgrey", paddingBottom:"12px"}}>
+          <button type="button" className="close" data-dismiss="modal" aria-label="Close" style={{marginRight:"5px", zIndex:0}}>
               <span aria-hidden="true">&times;</span>
             </button>
+            <h5 className="modal-title" id="exampleModalCenterTitle" style={{textAlign:"center", marginTop:"15px", width:"auto", zIndex:1}}>Define Your Event</h5>
           </div>
-          <div className="modal-body">
-            <div className="row"> 
-            <label>
-                Event Name
-              <input type="text"/>
-            </label>
-        </div>
-        <div>
-            <label>
-              Event Date
-              <input type="text"/>
-              </label>
-</div>
-<div>
-              <label>
-                Event Start Time
-                <input type="text"/>
-
-              </label>
-</div>
-<div>
-              <label>
-                Event End Time
-                <input type="text"/>
-              </label>
-
+          
+          <div style={{width:"100%", height:"60%", textAlign:"center", marginBottom:"10px"}}>
+            
+              <p className="label" style={{marginBottom:"0px"}}>Event Name</p>
+              <input style={{width:"auto", boxSizing:"border-box", borderRadius:"8px"}} type="text"/>
+             
+              <p className="label" style={{marginBottom:"0px"}}>Event Date</p>
+              <input style={{width:"auto", boxSizing:"border-box", borderRadius:"8px"}} type="text"/>
+            
+              <p className="label" style={{marginBottom:"0px"}}>Event Start Time</p>
+              <input style={{width:"auto", boxSizing:"border-box", borderRadius:"8px"}} type="text"/>
+             
+              <p className="label" style={{marginBottom:"0px"}}>Event End Time</p>
+              <input style={{width:"auto", boxSizing:"border-box", borderRadius:"8px"}} type="text"/>
+              
           </div>
-
-
-          </div>
-          <div className="modal-footer">
-            <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-            <button type="button" className="btn btn-primary" onClick={writeUserData}>Save changes</button>
+          <div className="modal-footer" style={{marginTop:"10px"}}>
+          <button type="button" className="button" onClick={writeUserData} style={{marginTop:"15px", borderRadius:"8px"}}>Save changes</button>
+            <button type="button" className="button" data-dismiss="modal" style={{marginTop:"20px", borderRadius:"8px"}}>Close</button>
           </div>
         </div>
       </div>
