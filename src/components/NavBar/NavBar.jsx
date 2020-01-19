c0d1816a87ce69b4fea1248730c4a8984ee6f201
Avelardo Valdez <avelardoval@gmail.com>
2020-01-18 16:35:12 -0800

    Add signout button

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,16 +2,22 @@ import React from "react";
 import NavBarSettings from './NavBarSettings';
 import { AccountIcon, CalendarIcon, HomeIcon } from '../Icons/Icons';
 import './NavBar.css';
+import app from "../../firebase";
 
 const NavBar = () => {
 
     var isOnUserPage = 'account';
+
+    function signOut() {
+      app.auth().signOut();
+    }
 
   return (
     <nav className='navbar fixed-top navbar-light bg-light navbar-flex'>
         {isOnUserPage === 'account' && <div className='hidden-flex'>
             <NavBarSettings />
         </div>}
+        <button onClick={signOut}>Sign Out</button>
       <div className='navbar-brand title-flex'>
         Calendar App
       </div>
